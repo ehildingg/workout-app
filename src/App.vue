3ba4c7c26b70a3586e4ec16b1cfb2da71ee2ff67
ehilding <erik.hildingsson@gmail.com>
2022-05-02 18:07:59 +0200

    changed hannes row

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 <template>
   <nav>
     <p>Tobias is in the houseee</p>
-    <p>hannes was here</p>
+    <p>hannes is herejeaioujfnsio</p>
     <p>Erik was here</p>
     <p>Helené was here</p>
     <ul>
