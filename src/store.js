2043df30f67c4b43c6a7f926f620e6fc7eb438e2
Tobias Johnsson <tobb-on@hotmail.com>
2022-05-03 18:40:32 +0200

    Cleaning, css, routes, VUEX-state

--- src/store.js
+++ src/store.js
@@ -6,7 +6,13 @@ const mutations = {
     }
   },
   state = {
-    counter: 0
+    counter: 0,
+    exerciseList: [
+      { id: 1, blockName: 'pullups', seconds: 6, resting: true, color: 'blue' },
+      { id: 2, blockName: 'rest', seconds: 5, resting: false, color: 'red' },
+      { id: 3, blockName: 'jump', seconds: 6, resting: true, color: 'green' },
+      { id: 4, blockName: 'rest', seconds: 5, resting: false, color: 'red' }
+    ]
   }
 
 export default createStore({ mutations, state, strict: true })
