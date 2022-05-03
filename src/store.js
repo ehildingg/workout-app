3104ccd04c0b09df9e6fa337ef5cae9a61875a13
Tobias Johnsson <tobb-on@hotmail.com>
2022-05-03 16:05:35 +0200

    update

--- src/store.js
+++ src/store.js
@@ -6,7 +6,13 @@ const mutations = {
     }
   },
   state = {
-    counter: 0
+    counter: 0,
+    exerciseList: [
+      { blockName: 'pullups', seconds: 6, resting: true, color: 'blue' },
+      { blockName: 'rest', seconds: 5, resting: false, color: 'red' },
+      { blockName: 'crunches', seconds: 6, resting: true, color: 'green' },
+      { blockName: 'rest', seconds: 5, resting: false, color: 'red' }
+    ]
   }
 
 export default createStore({ mutations, state, strict: true })
