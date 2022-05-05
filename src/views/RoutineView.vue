f87beaeb4b398d89871c893250a48d40fa23e1c4
Tobias Johnsson <tobb-on@hotmail.com>
2022-05-05 16:25:00 +0200

    Timer added

--- src/views/RoutineView.vue
+++ src/views/RoutineView.vue
@@ -1,72 +1,135 @@
 <script>
   export default {
     components: {},
-    created() {},
-    data() {
-      return {
-        hej: null
+    created() {
+      this.initData()
+    },
+    beforeUnmount() {
+      this.$timer.stop('exerciseTimer')
+    },
+    timers: {
+      exerciseTimer: {
+        time: 1000,
+        autostart: true,
+        repeat: true
       }
     },
-    computed: {
-      // Get exercisesArray from vuex
-      routineList: function () {
-        return this.$store.state.routineList
-      },
-      // Get data from router
-      getRoutePathName: function () {
-        return this.$route.fullPath
+    data() {
+      return {
+        exerciseArrayIds: null,
+        exerciseArray: [],
+        totalCounter: null,
+        counterInSeconds: null,
+        currentExercise: 0
       }
     },
     methods: {
-      hejFunction() {
-        console.log('hej')
+      exerciseTimer() {
+        console.log('ÖvningsTimer: ' + this.counterInSeconds)
+        this.counterInSeconds--
+        this.totalCounter--
+        console.log('Total Timer:' + this.totalCounter)
+
+        if (this.counterInSeconds == 0 && this.totalCounter != 0) {
+          this.$timer.stop('exerciseTimer')
+          this.prepareNextExercise()
+          this.startNextExercise()
+        }
+        if (this.totalCounter <= 0) {
+          this.finishWorkout()
+        }
+      },
+
+      prepareNextExercise() {
+        this.currentExercise++
+        console.log(this.exerciseArray[this.currentExercise].blockName)
+        this.counterInSeconds =
+          this.exerciseArray[this.currentExercise].durationInSeconds
+      },
+
+      startNextExercise() {
+        this.$timer.start('exerciseTimer')
+      },
+      finishWorkout() {
+        this.$timer.stop('exerciseTimer')
+      },
+
+      playPauseBtnClick() {
+        if (this.totalCounter > 0) {
+          this.timers.exerciseTimer.isRunning
+            ? this.$timer.stop('exerciseTimer')
+            : this.$timer.start('exerciseTimer')
+          console.log(this.timers.exerciseTimer.isRunning)
+        } else {
+          console.log('Exercise is done')
+        }
+      },
+      initData() {
+        //Router index
+        let routerString = '0'
+        console.log(this.$store.state.routineList[0].exercises)
+        this.exerciseArrayIds = this.$store.state.routineList[0].exercises
+        console.log(this.exerciseArrayIds)
+
+        this.exerciseArrayIds.forEach((id) => {
+          this.exerciseArray.push(this.$store.state.exerciseList[id])
+        })
+
+        console.log(this.exerciseArray)
+
+        this.exerciseArray.forEach((exercise) => {
+          this.totalCounter += exercise.seconds
+        })
+        console.log(this.totalCounter)
+
+        this.counterInSeconds = this.exerciseArray[0].seconds
+        console.log(this.exerciseArray[0].seconds)
       }
-    }
+    },
+    computed: {
+      totalTimeLeft: function () {
+        return new Date(this.totalCounter * 1000).toISOString().slice(14, 19)
+      }
+    },
+    props: {}
   }
 </script>
 
 <template>
-  <h1>RouterPath: {{ getRoutePathName }}</h1>
-  <h3>ROUTINES</h3>
-  <ol class="list-container" v-if="routineList">
-    <li
-      class="list-item"
-      :key="routineId"
-      v-for="(routineId, index) in routineList"
-    >
-      NAME: {{ $store.state.routineList[index].blockName }} <br />
-      ROUTINE-TIME:
-      {{ $store.state.routineList[index].seconds }} sec<br />
-    </li>
+  <div v-if="exerciseArray">
+    <h1>{{ this.exerciseArray[this.currentExercise].blockName }}</h1>
+    <h1>{{ this.counterInSeconds }}</h1>
+    <h1>Totaltimer:</h1>
+    <h1>{{ this.totalCounter }}</h1>
+    <p>{{ totalTimeLeft }}</p>
+  </div>
 
-    <!-- RENDERAR EXERCISES -->
-
-    <!-- <li
-      class="list-item"
-      :key="exerciseId"
-      v-for="exerciseId in routineList[0].exercises"
-    >
-      ID: {{ $store.state.exerciseList[exerciseId].id }}<br />
-      NAME: {{ $store.state.exerciseList[exerciseId].blockName }}<br />
-      INTERVAL: {{ $store.state.exerciseList[exerciseId].seconds }} sec<br />
-      REST: {{ $store.state.exerciseList[exerciseId].resting }}<br />
-      COLOR: {{ $store.state.exerciseList[exerciseId].color }}<br />
-      EXCERCISES: {{ $store.state.exerciseList[exerciseId] }}<br />
-
-    </li> -->
-  </ol>
+  <div class="dot-container">
+    <span :key="exercise.id" v-for="exercise in exerciseArray" />
+  </div>
+  <button @click="playPauseBtnClick()">Pause</button>
 </template>
 
 <style scoped>
-  /* SCOOPED STYLE/CSS, GÄLLER BARA DENNA KOMPONENTEN
-GLOBALA STYLES I APP.VUE */
-
-  .list-container {
+  .dot-container {
+    overflow: scroll;
+    white-space: nowrap;
+  }
+  .dot-active {
+    opacity: 1;
+  }
+  .dot-inactive {
+    opacity: 0.5;
+  }
+  span {
+    display: inline-block;
+    width: 2rem;
+    height: 2rem;
+    margin: 5px;
+    border-radius: 50%;
+    background-color: rgb(41, 82, 119);
+  }
+  .div {
     border: 1px solid black;
   }
-  .list-item {
-    border: 1px solid gray;
-    margin: 0.3rem;
-  }
 </style>
--->
